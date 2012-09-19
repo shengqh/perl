f62e47403e6b77f962480344a18aa853ff283985
shengqh@gmail.com <shengqh@gmail.com@1b7053f4-6955-7832-7b86-369546491a52>
2012-09-19 18:45:50 +0000

    pbs

---
+++ lib/CQS/PBS.pm
@@ -0,0 +1,61 @@
+#!/usr/bin/perl
+package CQS::PBS;
+
+use strict;
+use warnings;
+use File::Basename;
+
+require Exporter;
+
+our @ISA = qw(Exporter);
+
+our %EXPORT_TAGS = ( 'all' => [qw(init_dir get_pbs_desc)] );
+
+our @EXPORT = ( @{ $EXPORT_TAGS{'all'} } );
+
+our $VERSION = '0.01';
+
+sub init_dir {
+  my $rootDir = shift;
+
+  #defined several folders
+  my $pbsDir    = "$rootDir/pbs";
+  my $resultDir = "$rootDir/result";
+  my $logDir    = "$rootDir/log";
+
+  if ( !( -d $rootDir ) ) {
+    system "mkdir $rootDir";
+  }
+  if ( !( -d $pbsDir ) ) {
+    system "mkdir $pbsDir";
+  }
+  if ( !( -d $resultDir ) ) {
+    system "mkdir $resultDir";
+  }
+  if ( !( -d $logDir ) ) {
+    system "mkdir $logDir";
+  }
+
+  return ( $logDir, $pbsDir, $resultDir );
+}
+
+sub get_pbs_desc {
+  my $pbsDesc = <<PBS;
+#!/bin/bash
+#Beginning of PBS bash script
+#PBS -M quanhu.sheng\@vanderbilt.edu
+#Status/Progress Emails to be sent
+#PBS -m bae
+#Email generated at b)eginning, a)bort, and e)nd of jobs
+#PBS -l mem=15000mb
+#Total job memory required (specify how many megabytes)
+#PBS -l walltime=240:00:00
+#You must specify Wall Clock time (hh:mm:ss) [Maximum allowed 30 days = 720:00:00]
+#PBS -q batch
+PBS
+
+  return ($pbsDesc);
+}
+
+1;
+
---
+++ lib/CQS/RNASeq.pm
@@ -0,0 +1,104 @@
+#!/usr/bin/perl
+package CQS::RNASeq;
+
+use strict;
+use warnings;
+use File::Basename;
+use CQS::PBS;
+
+require Exporter;
+
+our @ISA = qw(Exporter);
+
+our %EXPORT_TAGS = ( 'all' => [qw(tophat2_by_pbs_single tophat2_by_pbs_double)] );
+
+our @EXPORT = ( @{ $EXPORT_TAGS{'all'} } );
+
+our $VERSION = '0.01';
+
+use Cwd;
+
+sub tophat2_by_pbs_single {
+  my ( $faFile, $sampleFile, $rootDir ) = @_;
+
+  my ( $sampleName, $directories, $suffix ) = fileparse($sampleFile);
+
+  $sampleName =~ s/\.(\w+)$//;
+
+  my $pathFile = '/home/shengq1/bin/path.txt';
+
+  my ( $logDir, $pbsDir, $resultDir ) = init_dir($rootDir);
+
+  my ($pbsDesc) = get_pbs_desc();
+
+  my $saiFile       = $sampleName . ".sai";
+  my $samFile       = $sampleName . ".sam";
+  my $bamFile       = $sampleName . ".bam";
+  my $sortedBamFile = $sampleName . "_sort";
+  my $log           = $logDir . "/" . $sampleName . ".log";
+
+  if ( -e $resultDir . "/" . $sortedBamFile ) {
+    next;
+  }
+
+  #my $tag="'\@RG\tID:$sample\tLB:$sample\tSM:$sample\tPL:ILLUMINA'";
+  my $pbsFile = $pbsDir . "/${sampleName}.pbs";
+  print "$pbsDir\n";
+  open( OUT, ">$pbsFile" ) or die $!;
+  print OUT $pbsDesc;
+  print OUT "#PBS -o $log\n";
+  print OUT "#PBS -j oe\n\n";
+  print OUT "source $pathFile\n";
+  print OUT "cd $resultDir\n\n";
+
+  if ( !( -e $resultDir . "/" . $bamFile ) ) {
+    if ( !( -e $resultDir . "/" . $samFile ) ) {
+      if ( !( -e $resultDir . "/" . $sampleFile ) ) {
+        print OUT "echo sai=`date` \n";
+        print OUT "bwa aln -q 15 $faFile $sampleFile >$saiFile \n";
+      }
+      print OUT "echo aln=`date` \n";
+      print OUT "bwa sampe -n 3 $faFile $saiFile $sampleFile > $samFile \n";
+    }
+    print OUT "echo sam2bam=`date`\n";
+    print OUT "samtools view -b -S $samFile -o $bamFile\n";
+  }
+  print OUT "echo sortbam=`date`\n";
+  print OUT "samtools sort $bamFile $sortedBamFile\n";
+  print OUT "echo bamstat=`date`\n";
+  print OUT "samtools flagstat $sortedBamFile.bam > $sortedBamFile.bam.stat\n";
+  print OUT "echo finished=`date`\n";
+  close OUT;
+
+  `qsub $pbsFile`;
+}
+
+sub tophat2_by_pbs_double {
+  my ( $dbDir, $sampleFile1, $sampleFile2, $sampleName, $rootDir ) = @_;
+
+  my $pathFile = '/home/shengq1/bin/path.txt';
+
+  my ( $logDir, $pbsDir, $resultDir ) = init_dir($rootDir);
+
+  my ($pbsDesc) = get_pbs_desc();
+
+  my $log           = $logDir . "/" . $sampleName . ".log";
+
+  my $pbsFile = $pbsDir . "/${sampleName}.pbs";
+  print "$pbsDir\n";
+  open( OUT, ">$pbsFile" ) or die $!;
+  print OUT $pbsDesc;
+  print OUT "#PBS -o $log\n";
+  print OUT "#PBS -j oe\n\n";
+  print OUT "source $pathFile\n";
+  print OUT "cd $resultDir\n\n";
+
+  print OUT "echo tophat2=`date` \n";
+  print OUT "tophat2 --segment-length 25 -r 0 -p 8 -o $resultDir $dbDir $sampleFile1 $sampleFile2\n";
+  print OUT "echo finished=`date`\n";
+  close OUT;
+
+  `qsub $pbsFile`;
+}
+
+1;
