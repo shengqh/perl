d2ed68a47848680ce8518f4f8a44da21146e3a6b
shengqh@gmail.com <shengqh@gmail.com@1b7053f4-6955-7832-7b86-369546491a52>
2012-09-20 21:05:27 +0000

    rnaseq

--- trunk/Test/TestRNASeqPipeline.pl
+++ trunk/Test/TestRNASeqPipeline.pl
@@ -5,31 +5,31 @@ use warnings;
 use CQS::QC;
 use CQS::RNASeq;
 
-my @samples = ("1","3","4","5","10","11","13","16");
+my @samples = ( "1", "3", "4", "5", "10", "11", "13", "16" );
 
-my $dbFile = "/data/cqs/guoy1/reference/hg19/bowtie2_index/hg19";
-my $rootDir = "/scratch/cqs/shengq1/rnaseq/1769";
+my $genomeDb     = "/data/cqs/guoy1/reference/hg19/bowtie2_index/hg19";
+my $rootDir      = "/scratch/cqs/shengq1/rnaseq/1769";
+my $gtfFile      = "/home/liuq6/reference/hg19.ensembl-for-tophat.gtf";
+my $gtfIndex     = "/scratch/cqs/shengq1/gtfindex/hg19";
+my $tophat2param = "--segment-length 25 -r 0 -p 8";
 
-unless(-e $rootDir or mkdir($rootDir)){
-  die "Unable to create $rootDir\n";
+unless ( -e $rootDir or mkdir($rootDir) ) {
+	die "Unable to create $rootDir\n";
 }
 
+my @sampleNames = ();
+my @sampleFiles = ();
+
 foreach my $sample (@samples) {
-  my $name = "1769-DPC-" . $sample;	
+	my $name = "1769-DPC-" . $sample;
 
-  my $fastqFile1 = "/scratch/cqs/guoy1/1769/rawdata/" . $name . "_1_sequence.txt";
-	
-  my $fastqFile2 = "/scratch/cqs/guoy1/1769/rawdata/" . $name . "_2_sequence.txt";
-	
-  my $curDir = $rootDir . "/" . $name;
-	
-  unless(-e $curDir or mkdir($curDir)){
-    die "Unable to create $curDir\n";
-  }
+	my $fastqFile1 = "/scratch/cqs/guoy1/1769/rawdata/" . $name . "_1_sequence.txt";
 
-  fastqc_by_pbs($curDir, $name . "_1", $fastqFile1);
+	my $fastqFile2 = "/scratch/cqs/guoy1/1769/rawdata/" . $name . "_2_sequence.txt";
 
-  fastqc_by_pbs($curDir, $name . "_2", $fastqFile2);
+	push( @sampleNames, $name );
+	push( @sampleFiles, $fastqFile1 );
+	push( @sampleFiles, $fastqFile2 );
 
-  #tophat2_by_pbs_double( $dbFile, $curDir, $name, $fastqFile1, $fastqFile2 );
+	tophat2_by_pbs( $genomeDb, $gtfFile, $gtfIndex, $tophat2param, $rootDir, "test1769", \@sampleNames, \@sampleFiles );
 }
