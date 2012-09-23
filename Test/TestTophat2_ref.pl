65f92888215e9697fa5f8e20ffcd241b3db1594c
shengqh@gmail.com <shengqh@gmail.com@1b7053f4-6955-7832-7b86-369546491a52>
2012-09-23 01:40:01 +0000

    tophat

---
+++ Test/TestTophat2_ref.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+use CQS::QC;
+use CQS::RNASeq;
+use CQS::FileUtils;
+use CQS::SystemUtils;
+
+my @samples = ( "1", "3", "4", "5", "10", "11", "13", "16" );
+
+my $genomeDb     = "/data/cqs/guoy1/reference/hg19/bowtie2_index/hg19";
+my $rootDir      = "/scratch/cqs/shengq1/rnaseq/1769_2";
+my $gtfFile      = "/data/cqs/guoy1/reference/annotation2/hg19/Homo_sapiens.GRCh37.68.gtf";
+my $gtfIndex     = "/scratch/cqs/shengq1/gtfindex/hg19_GRCh37_68";
+my $tophat2param = "--segment-length 25 -r 0 -p 8";
+
+my $runNow = get_run_now();
+
+create_directory_or_die($rootDir);
+
+my @sampleNames = ();
+my @sampleFiles = ();
+
+foreach my $sample (@samples) {
+	my $name = "1769-DPC-" . $sample;
+
+	my $fastqFile1 = "/scratch/cqs/guoy1/1769/rawdata/" . $name . "_1_sequence.txt";
+
+	my $fastqFile2 = "/scratch/cqs/guoy1/1769/rawdata/" . $name . "_2_sequence.txt";
+
+	push( @sampleNames, $name );
+	push( @sampleFiles, $fastqFile1 );
+	push( @sampleFiles, $fastqFile2 );
+}
+
+#tophat2_by_pbs_batch( $genomeDb, $gtfFile, $gtfIndex, $tophat2param, $rootDir, "test1769", \@sampleNames, \@sampleFiles );
+tophat2_by_pbs_individual( $genomeDb, $gtfFile, $gtfIndex, $tophat2param, $rootDir, \@sampleNames, \@sampleFiles, $runNow );
