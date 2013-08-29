ea2bcae84d0d0d6e1fed9b1aef284805fa2feefa
shengqh@gmail.com <shengqh@gmail.com@1b7053f4-6955-7832-7b86-369546491a52>
2013-08-29 14:46:52 +0000

    dos2unix

--- trunk/bin/buildsmad4db.sh
+++ trunk/bin/buildsmad4db.sh
@@ -1,5 +1,5 @@
-cd /data/cqs/shengq1/reference/mm10/smad4 
+cd /data/cqs/shengq1/reference/mm10/smad4
 
-grep Smad4 /data/cqs/guoy1/reference/annotation2/mm10/Mus_musculus.GRCm38.68.gtf > smad4.gtf 
+grep Smad4 /data/cqs/guoy1/reference/annotation2/mm10/Mus_musculus.GRCm38.68.gtf > smad4.gtf
 
 python /home/shengq1/pylibs/bin/dexseq_prepare_annotation.py smad4.gtf smad4.gff 
