4fd2fd90943cfcaa0e5b763f5a206f66ee977408
Quanhu Sheng <shengqh@gmail.com>
2016-05-06 12:42:47 -0500

    download tcga

---
+++ bin/download_tcga.sh
@@ -0,0 +1,17 @@
+#cgquery "legacy_sample_id=TCGA-AG-3580-01A-22-2150-27"
+#gtdownload -c ~/.ssh/cghub.key -d cb1bdc67-5c8b-4324-84c9-b2b293644056
+
+#gtdownload -c ~/.ssh/cghub.key -d 026e62f8-1de2-40c5-915f-7358cf009ad1
+
+gtdownload -c ~/.ssh/cghub.key -d 2953f81c-73b8-460a-a1e0-e5cc28474285
+
+gtdownload -c ~/.ssh/cghub.key -d 8b03c6f7-1697-42a7-b7f2-b0e37de97a85
+
+gtdownload -c ~/.ssh/cghub.key -d f9634481-02e3-412d-881f-acb423edb9ce
+
+gtdownload -c ~/.ssh/cghub.key -d cb1bdc67-5c8b-4324-84c9-b2b293644056
+
+gtdownload -c ~/.ssh/cghub.key -d 17f868e1-b812-4ccd-92d2-ee5063d17355
+
+gtdownload -c ~/.ssh/cghub.key -d 8ea0472d-6a63-4044-9c36-dd658b0a7b9f
+
